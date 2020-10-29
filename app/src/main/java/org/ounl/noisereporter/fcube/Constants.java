88a62c3218e63ebea614ee6846a798a67a4eae5f
btabuenca <bernardo.tabuenca@gmail.com>
2020-10-29 11:08:20 +0100

    Base versions

---
+++ app/src/main/java/org/ounl/noisereporter/fcube/Constants.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+  * Copyright (C) 2014 Open University of The Netherlands
+ * Author: Bernardo Tabuenca Archilla
+ * LearnTracker project 
+ * 
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package org.ounl.noisereporter.fcube;
+
+/**
+ * @author Bernardo Tabuenca
+ * 
+ */
+public class Constants {
+
+
+	public final static int NOISE_LEVEL_INIT = -1;
+
+	
+
+}
